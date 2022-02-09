377e384c415262fe180ec9fd1360b2a0f152392d
Felipe-MSC <felipe.da.silva2@ibm.com>
2022-02-09 11:04:16 -0300

    First Commit

---
+++ src/components/Slider.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import SliderContent from "./SliderContent";
+import Dots from "./Dots";
+import Arrows from "./Arrows";
+import sliderImage from "./sliderImage";
+import { FaPlayCircle, FaPauseCircle } from "react-icons/fa";
+import "./slider.css";
+
+const len = sliderImage.length - 1;
+
+function Slider(props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [playFrame, setPlayFrame] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex(activeIndex === len ? 0 : activeIndex + 1);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [activeIndex]);
+
+  return (
+    <div className="slider-container">
+      {playFrame ? (
+        <FaPauseCircle
+          className="fa-icon"
+          onClick={() => setPlayFrame(!playFrame)}
+        />
+      ) : (
+        <FaPlayCircle
+          className="fa-icon"
+          onClick={() => setPlayFrame(!playFrame)}
+        />
+      )}
+      <SliderContent activeIndex={activeIndex} sliderImage={sliderImage} />
+      <Arrows
+        prevSlide={() =>
+          setActiveIndex(activeIndex < 1 ? len : activeIndex - 1)
+        }
+        nextSlide={() =>
+          setActiveIndex(activeIndex === len ? 0 : activeIndex + 1)
+        }
+      />
+      <Dots
+        activeIndex={activeIndex}
+        sliderImage={sliderImage}
+        onclick={(activeIndex) => setActiveIndex(activeIndex)}
+      />
+    </div>
+  );
+}
+
+export default Slider;
---
+++ src/components/sliderImage.js
@@ -0,0 +1,33 @@
+import First from "../assets/First.jpg";
+import Second from "../assets/Second.jpg";
+import Third from "../assets/Third.jpg";
+import Fourth from "../assets/Fourth.jpeg";
+import Fifth from "../assets/Fifth.jpeg";
+
+export default [
+  {
+    title: "First Frame",
+    description: "This is the first frame Image of our carousel",
+    urls: First,
+  },
+  {
+    title: "Second Slide",
+    description: "This is the second frame Image of our carousel",
+    urls: Second,
+  },
+  {
+    title: "Third Slide",
+    description: "This is the Third frame Image of our carousel",
+    urls: Third,
+  },
+  {
+    title: "Fourth Slide",
+    description: "This is the Fourth frame Image of our carousel",
+    urls: Fourth,
+  },
+  {
+    title: "Fifth Slide",
+    description: "This is the Fifth frame Image of our carousel",
+    urls: Fifth,
+  },
+];
